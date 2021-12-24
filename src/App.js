b9c932e53c6df2d97f05ad74a82b124c63aa3735
Jude Dickson <justclassic24@gmail.com>
2021-12-24 14:04:41 +0100

    fixed update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
-import Starter from './pages/Starter';
 import CreateRoom from "./routes/CreateRoom";
 import Room from "./routes/Room";
 
@@ -7,7 +6,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path= '/instruct' element={<Starter/>} />
         <Route path="/" exact element={<CreateRoom/>} />
         <Route path="/room/:roomID" element={<Room/>} />
       </Routes >
